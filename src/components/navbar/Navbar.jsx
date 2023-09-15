b5c38da9625d7efc761eb7ddbfc6138a6404bc05
roddelapan <j2001.dlrr@gmail.com>
2023-09-14 18:33:01 -0700

    Fixed home styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,8 +12,8 @@ export const Navbar = () => {
             <nav>
                 <span className={styles.toggle_button} onClick={() => {is_expanded == null ? setExpanded(true) : setExpanded(!is_expanded)}}>
                     Site index
-                    <svg display={is_expanded ?  "block" : "none"} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" /></svg>
-                    <svg display={is_expanded ?  "none" : "block"} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" /></svg>
+                    <svg display={is_expanded ?  "none" : "block"} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" /></svg>
+                    <svg display={is_expanded ?  "block" : "none"} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" /></svg>
 
                 </span>
                 <div className={[styles.index_container]} expanded={is_expanded != null ? Boolean(is_expanded).toString() : undefined}>
